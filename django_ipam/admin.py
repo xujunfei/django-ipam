f5a8da878e21320237bae33759fcee80f48bdab1
Steed@263 <xusteed@163.com>
2020-11-11 18:04:26 +0800

    增加客户线路表

--- django_ipam/admin.py
+++ django_ipam/admin.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Create: 2020-11-11 13:25:56
+# LastEdit: 2020-11-11 17:58:49
+"""说明暂无"""
+__author__ = '749B'
+
+
 from django.contrib import admin
 
 from .base.admin import AbstractIpAddressAdmin, AbstractSubnetAdmin
-from .models import IpAddress, Subnet
+from .models import IpAddress, Subnet, Product
+
+from openwisp_utils.admin import TimeReadonlyAdminMixin
 
 
 @admin.register(IpAddress)
@@ -11,4 +21,27 @@ class IPAddressAdmin(AbstractIpAddressAdmin):
 
 @admin.register(Subnet)
 class BaseSubnet(AbstractSubnetAdmin):
-    pass
+    list_display = ('__str__', 'master_subnet', 'subnet_class2', 'product2', 'description')
+
+    def subnet_class2(self, obj, lv=''):
+        """递归获取父级网络的IP分类(subnet_class)"""
+        name = obj.subnet_class
+        if name:
+            return lv + dict(obj.subnet_class_choices).get(name, "未知选项")
+        if obj.master_subnet:
+            lv += '*'
+            return self.subnet_class2(obj.master_subnet, lv)
+    subnet_class2.short_description = "IP分类(继承)"
+    subnet_class2.empty_value_display = "没有继承"
+
+    def product2(self, obj):
+        return obj.product
+    product2.short_description = "绑定线路"
+    product2.empty_value_display = "—未分配—"
+
+
+@admin.register(Product)
+class ProductAdmin(TimeReadonlyAdminMixin, admin.ModelAdmin):
+    list_display = ('__str__', 'description', )
+
+
--- django_ipam/models.py
+++ django_ipam/models.py
@@ -2,8 +2,20 @@ from swapper import swappable_setting
 
 from .base.models import AbstractIpAddress, AbstractSubnet
 
+from django.db import models
+from openwisp_utils.base import TimeStampedEditableModel
 
 class Subnet(AbstractSubnet):
+    subnet_class_choices = (
+        ("BGP", "BGP"),
+        ("CT", "电信"),
+        ("CU", "联通"),
+        ("CM", "移动"),
+        ("LOCAL", "私网地址"),
+    )
+    subnet_class = models.CharField(max_length=10, blank=True, choices=subnet_class_choices, verbose_name="IP分类")
+    product = models.ForeignKey('Product', models.CASCADE, blank=True, null=True)
+
     class Meta(AbstractSubnet.Meta):
         abstract = False
         swappable = swappable_setting('django_ipam', 'Subnet')
@@ -17,3 +29,16 @@ class IpAddress(AbstractIpAddress):
 
 class CsvImportException(Exception):
     pass
+
+
+class Product(TimeStampedEditableModel):
+    customer_name = models.CharField(max_length=100, blank=True, verbose_name="客户名称")
+    order_id = models.CharField(max_length=100, blank=True, verbose_name="订单编号")
+    description = models.CharField(max_length=100, blank=True, verbose_name="描述")
+
+    class Meta:
+        verbose_name = "客户线路"
+        verbose_name_plural = "客户线路表"
+
+    def __str__(self):
+        return "%s(%s)" % (self.customer_name, self.order_id)
